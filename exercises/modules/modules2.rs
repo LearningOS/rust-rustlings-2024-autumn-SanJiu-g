8a5b15491eacc8b5e7782e44f580ee9a49ba8f8c
SanJiu-g <3490346972@qq.com>
2024-10-05 05:14:56 -0700

    solve modules1,2,3

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,12 +7,15 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    use self::fruits::PEAR as fruit_value;
+    use self::veggies::CUCUMBER as veggie_value;
+
+    pub const fruit: &'static str = fruit_value;
+    pub const veggie: &'static str = veggie_value;
+
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
